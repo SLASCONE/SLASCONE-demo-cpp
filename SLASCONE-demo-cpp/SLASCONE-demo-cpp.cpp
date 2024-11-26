9aee41947a2c2fa68403c0bd6e72b4e5417ef89d
Michael Dürr <michael@slascone.com>
2024-11-26 15:41:23 +0000

    Init SLASCONE-demo

---
+++ SLASCONE-demo-cpp/SLASCONE-demo-cpp.cpp
@@ -0,0 +1,12 @@
+﻿// SLASCONE-demo-cpp.cpp : Defines the entry point for the application.
+//
+
+#include "SLASCONE-demo-cpp.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
